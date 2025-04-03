304301e2af4f21fdc0f41bb54cd4fa45328bb713
jamesharrold9 <20429915@student.curtin.edu.au>
2025-04-03 14:45:00 +0800

    Add first step for stats

--- goostats.sh
+++ goostats.sh
@@ -3,9 +3,6 @@ fname=$1
 echo "working with file:"
 echo "welcome to James'stats script"
 # Compute the min/max/range of values in a file
-<<<<<<< HEAD
 min=$( cat${fname} | sort | head -1)
-
-=======
 max=$( cat ${fname} | sort | tail -1)
->>>>>>> 5ed32a9471f05439b4df1533662344a5d7722cbc
+
